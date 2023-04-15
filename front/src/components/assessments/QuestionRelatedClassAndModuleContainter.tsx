99de72453e4ff418f05e7ee142658b8aa80a74b3
Mamadou <20203997@etud.univ-evry.fr>
2023-04-15 12:22:11 +0200

    create empty component files for up coming work

---
+++ front/src/components/assessments/QuestionRelatedClassAndModuleContainter.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const QuestionRelatedClassAndModuleContainter = () => {
+  return <div>QuestionRelatedClassAndModuleContainter</div>
+}
+
+export default QuestionRelatedClassAndModuleContainter
