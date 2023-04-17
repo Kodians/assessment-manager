2b2413f62d046bc76e96c522c3dc77e637335fdf
Danko Konate <dankokonate@gmail.com>
2023-04-17 15:49:58 +0200

    resolve

--- front/src/components/modules/addAndEditModuleForm.tsx
+++ front/src/components/modules/addAndEditModuleForm.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 
-import { Button, CardActions, CardContent, Divider, TextField } from '@mui/material'
+import {
+  Button,
+  CardActions,
+  CardContent,
+  Chip,
+  Divider,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from '@mui/material'
 import Card from '@mui/material/Card'
 
 export const AddAndEditModuleForm = () => {
   const [name, setName] = React.useState({
     label: '',
     description: '',
+    classe: '',
   })
 
   const handleLabelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,10 +31,16 @@ export const AddAndEditModuleForm = () => {
     setName({ ...name, description: value })
   }
 
+  const handleClasseChange = (event: any) => {
+    const value = event.target.value as string
+    setName({ ...name, classe: value })
+  }
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    setName({ label: '', description: '' })
+    setName({ label: '', description: '', classe: '' })
   }
+
   const onClose = () => void 0
 
   return (
@@ -49,6 +67,28 @@ export const AddAndEditModuleForm = () => {
               rows={4}
               sx={{ mb: 1 }}
             />
+            <FormControl sx={{ width: '450px', marginBottom: 1 }}>
+              <InputLabel id="classe-label">Classe</InputLabel>
+              <Select
+                labelId="classe-label"
+                id="classe"
+                value={name.classe}
+                label="Classe"
+                onChange={handleClasseChange}
+                renderValue={(selected) => (
+                  <div>
+                    {(selected as unknown as string[]).map((value) => (
+                      <Chip key={value} label={value} sx={{ marginRight: 0.5 }} />
+                    ))}
+                  </div>
+                )}
+                multiple
+              >
+                <MenuItem value="1">1</MenuItem>
+                <MenuItem value="2">2</MenuItem>
+                <MenuItem value="3">3</MenuItem>
+              </Select>
+            </FormControl>
             <Divider />
             <CardActions sx={{ display: 'flex', justifyContent: 'flex-end', mb: 1, mr: 1 }}>
               <Button variant="contained" onClick={onClose} sx={{ mr: 1, backgroundColor: 'gray' }}>
